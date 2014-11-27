12adc275acbaa24b6d88725f948352e4ab1aa45b
weeniearms <weeniearms@gmail.com>
2014-11-27 20:31:02 +0100

    Added basic project structure

---
+++ src/main/java/org/sausage/picar/App.java
@@ -0,0 +1,17 @@
+package org.sausage.picar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigurationProperties
+@ComponentScan
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
+    }
+}
